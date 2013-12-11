72d7b0032de92b5ed3f3833bc63c0231812cec24
Dan Schuman <danschuman@gmail.com>
2013-12-11 12:25:06 -0800

    Basic features

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,7 @@
 class Currency < ActiveRecord::Base
+  validates_presence_of :rate,:country
+
+  def convert_to_base amount
+    amount * self.rate
+  end
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Currency do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @currency = create(:currency)
+  end
+
+  it "Validates presence of rate" do
+    @currency.rate = nil
+    @currency.should_not be_valid
+    @currency.rate = 123.456
+    @currency.should be_valid
+  end
+
+  it "Validates precense of country" do
+    @currency.country = nil
+    @currency.should_not be_valid
+    @currency.country = 'EUR'
+    @currency.should be_valid
+  end
+
+  it "Converts into the base currency" do
+    @currency.rate = 2.5
+    @currency.convert_to_base(100).should eq(250)
+    @currency.rate = 0.333
+    @currency.convert_to_base(77).should eq(25.641)
+  end
+
 end
