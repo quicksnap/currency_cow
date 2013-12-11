dbddf69a9a5f7fbbc1f30929afdf17e134236037
Dan Schuman <danschuman@gmail.com>
2013-12-11 12:08:16 -0800

    Rails 4 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurrencyCow::Application.config.secret_key_base = '8c35e98e7c6292bc22aaac57cf7a8dd59ea91408df48dfa9a6d0d4c3b248f4de5eefef2ce2592b39025f379f8bdb7db44d1c4dc67c5b6bf6d3b94509be212471'
